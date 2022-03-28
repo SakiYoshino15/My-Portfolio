84094f4f463904e7f7fd05ba0a436937467e83b0
吉野早紀 <xihuan15caomei@gmail.com>
2022-03-28 17:15:40 +0900

    create: back環境構築

---
+++ portfolio-vue/.eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  root: true,
+
+  env: {
+    node: true
+  },
+
+  extends: [
+    'plugin:vue/essential',
+    '@vue/standard',
+    '@vue/typescript',
+    '@vue/typescript/recommended',
+    '@vue/eslint-config-typescript'
+  ],
+
+  parserOptions: {
+    parser: '@typescript-eslint/parser'
+  },
+
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'space-before-function-paren': 'off',
+    'camelcase': 'off',
+    'vue/valid-v-slot': 'off',
+    'quotes': 'off',
+    'vue/multi-word-component-names': 'off'
+  },
+
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ]
+}
